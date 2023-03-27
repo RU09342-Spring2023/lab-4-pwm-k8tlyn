baa59e73ce1c442cdaabfd1f32a52f62b3c4b342
Kaitlyn Pounds <pounds78@students.rowan.edu>
2023-03-27 16:51:47 -0400

    Updated READme

--- 1. Software Defined PWM/README.md
+++ 1. Software Defined PWM/README.md
@@ -1,32 +1,9 @@
-# Software PWM
-Most microprocessors will have a Timer module, but depending on the device, some may not come with pre-built PWM modules. Instead, you may have to utilize software techniques to synthesize PWM on your own.
+# Software PWM Part 1
+There are three TB0CCRx registers, one for the period of the clk, and one for each duty cycle controlling the brightness of the LED. TB0 is using SMCLK in up mode. 
 
-## Task
-You need to generate a 1kHz PWM signal with a duty cycle between 0% and 100%. Upon the processor starting up, you should PWM both of the on-board LEDs at a 50% duty cycle. Upon pressing the on-board buttons, the duty cycle of the LEDs should increase by 10%, based on which button you press. Once you have reached 100%, your duty cycle should go back to 0% on the next button press.
- - Button 2.1 Should control LED 1.0
- - Button 4.3 should control LED 6.6
+*TB0CCR0 is set to 1000 so there is 1kHz frequency. 
+*TB0CCR1 and TB0CCR2 are set to 500 so that the LEDs have 50% duty cycle initially.
 
-## Deliverables
-You will need to upload the .c file and a README explaining your code and any design decisions made.
+The timer interrupt turns on both LEDs.
 
-### Hints
-You really, really, really, really need to hook up the output of your LED pin to an oscilloscope to make sure that the duty cycle is accurate. Also, since you are going to be doing a lot of initialization, it would be helpful for all persons involved if you created your main function like:
-
-```c
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	LEDSetup(); // Initialize our LEDS
-	ButtonSetup();  // Initialize our button
-	TimerA0Setup(); // Initialize Timer0
-	TimerA1Setup(); // Initialize Timer1
-	__bis_SR_register(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
-}
-```
-
-This way, each of the steps in initialization can be isolated for easier understanding and debugging.
-
-
-## Extra Work
-### Linear Brightness
-Much like every other things with humans, not everything we interact with we perceive as linear. For senses such as sight or hearing, certain features such as volume or brightness have a logarithmic relationship with our senses. Instead of just incrementing by 10%, try making the brightness appear to change linearly.
+There are two button interrupts which increment TB0CCR1 and TB0CCR2 by 100 every button press, to change the duty cycle by 10%. If the value in CCR1 or CCR2 goes above 1000, it wraps back around and becomes 0.
--- 2. Hardware Defined PWM/main.c
+++ 2. Hardware Defined PWM/main.c
@@ -3,16 +3,16 @@
  *
 You will need to use pins 6.0, 6.1, and 6.2 to drive an RGB LED.
 
-These will need to be configured with a PWM Period of 1ms.
+These will need to be configured with a PWM Period of 1ms. = 1kHz
 
  You need your RGB LED to cycle between the following colors in order:
 
-Red
-Orange (Red + Green)
-Green
-Cyan (Green + Blue)
-Blue
-Purple (Red + Blue)
+Red                   R = 50% G = 0%  B = 0%
+Orange (Red + Green)  R = 50% G = 50% B = 0%
+Green                 R = 0%  G = 50% B = 0%
+Cyan (Green + Blue)   R = 0%  G = 50% B = 50%
+Blue                  R = 0%  G = 0%  B = 50%
+Purple (Red + Blue)   R = 50% G = 0%  B = 50%
 
 You need to cover colors in between them, meaning as you transition from Red to Orange, it shouldn't be just 2 colors.
 
@@ -26,6 +26,7 @@ The timing for cycling is up to you to determine as well.
 
 void pinSetup();
 void TimerB0Setup();
+void changeColor();
 
 int main(void)
 {
@@ -36,48 +37,46 @@ int main(void)
         PM5CTL0 &= ~LOCKLPM5;
 
         pinSetup();
-        TimerB0Setup(); // Initialize Timer0
+        TimerB3Setup(); // Initialize Timer0
         __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
 
 }
 
 void pinSetup(){
-    P6OUT &= ~BIT0;
-    P6DIR |= BIT0;
+    P6OUT &= ~BIT0;         //set pins for RGB output
+    P6DIR |= BIT0;          //P6.0 uses TB3.1
     P6OUT &= ~BIT1;
-    P6DIR |= BIT1;
+    P6DIR |= BIT1;          //P6.1 uses TB3.2
     P6OUT &= ~BIT2;
-    P6DIR |= BIT2;
+    P6DIR |= BIT2;          //P6.2 uses TB3.3
 }
-void TimerB0Setup(){
-        // Configure Timer_B0
-            TB0CTL = TBSSEL_2 | MC__UP | TBCLR | TBIE;      // SMCLK, up mode, clear
-            TB0CCTL0 = OUTMOD_7;                          // CCR1 reset/set
+void TimerB3Setup(){
+        // Configure Timer_B3
+            TB3CTL = TBSSEL_2 | MC__UP | TBCLR | TBIE;      // SMCLK, up mode, clear
+            TB3CCTL1 = OUTMOD_7;                          // period reset/set
         //TBR, enable interrupt
-            TB0CCTL1 |= CCIE;                             // Enable TB0 CCR2 Interrupt
-            TB0CCTL2 |= CCIE;                             // Enable TB0 CCR3 Interrupt
-            TB0CCR0 = 1000;                               //1kHz frequency
-            TB0CCR1 = 500;                          //initial duty cycle (50%) for red LED
-            TB0CCR2 = 500;                          //same but for green LED
+            TB0CCTL1 |= CCIE;                             // Enable TB3CCR1 Interrupt
+            TB3CCR0 = 1000;                               //1kHz frequency
+            TB3CCR1 = 500;                                //50% duty cycle
+
+}
+void changeColor(){
+
 }
 
 // Timer0 B0 interrupt service routine
-#pragma vector = TIMER0_B1_VECTOR
-__interrupt void Timer0_B1_ISR(void)
+#pragma vector = TIMER3_B0_VECTOR
+__interrupt void Timer3_B0_ISR(void)
 {
     switch(__even_in_range(TB0IV,TB0IV_TBIFG))
       {
         case  0:                              //no interrupt pending
             break;
         case  TB0IV_TBCCR1:                   // CCR1
-            P1OUT &= ~BIT0;                   //set P1.0 to 0
-            break;
-        case  TB0IV_TBCCR2:                   // CCR2
-            P6OUT &= ~BIT6;                   //set P6.6 to 0
+
             break;
         case  TB0IV_TBIFG:                    // TBIFG
-            P1OUT ^= BIT0;                    //toggle red LED
-            P6OUT ^= BIT6;                    //toggle green LED
+            changeColor();                   //change color of light
             break;
         default:
             break;
